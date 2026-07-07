073e9a154e4da14c05886e8d69c90fb08c72c4e1
Chhavi Bakhru <25f1002355@ds.study.iitm.ac.in>
2026-07-07 11:32:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+import uuid
+import time
+
+app = FastAPI()
+
+ALLOWED_ORIGIN = "https://dash-mit5yl.example.com"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[ALLOWED_ORIGIN],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+EMAIL = "25f1002355@ds.study.iitm.ac.in" 
+
+
+class RequestMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        start = time.time()
+
+        response = await call_next(request)
+
+        response.headers["X-Request-ID"] = str(uuid.uuid4())
+        response.headers["X-Process-Time"] = str(time.time() - start)
+
+        return response
+
+
+app.add_middleware(RequestMiddleware)
+
+
+@app.get("/stats")
+async def stats(values: str = Query(...)):
+    nums = [int(x) for x in values.split(",")]
+
+    return {
+        "email": EMAIL,
+        "count": len(nums),
+        "sum": sum(nums),
+        "min": min(nums),
+        "max": max(nums),
+        "mean": sum(nums) / len(nums),
+    }
